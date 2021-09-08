a8fdb21c60c48aa251f46efa73132d8435d8fa23
David Jarillo <vjdavid621@gmail.com>
2021-09-08 14:49:36 -0700

    adding routes to redirect the data to proper views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get '/home', to: 'home#index'
-  post '/parse_url', to: 'parser#parse_url'
+  root 'home#index'
+
+  get ':code_url', to: 'shortened_urls#show', constraints: {
+    code_url: /\d{6}/
+  }
+  resources :shortened_urls do
+    get 'home', on: :collection
+  end
 end
