a45597001b1c53c5763bbbff91a0015fe7b156f6
David Jarillo <vjdavid621@gmail.com>
2021-09-08 12:14:10 -0700

    installing ValideUrl gem

---
+++ app/models/shortened_url.rb
@@ -0,0 +1,22 @@
+class ShortenedUrl < ApplicationRecord
+  HOST_URL = "#{ENV['DEFAULT_URL']}".freeze
+
+  validates_presence_of :full_url
+
+  before_create :set_code_url, :build_shortened_url
+
+  def self.short_or_create_from_url(full_url)
+    shortened_url = self.find_by(full_url: full_url)
+    shortened_url ||= self.create(full_url: full_url)
+  end
+
+  private
+
+  def set_code_url
+    self.code_url = SecureRandom.hex(3)
+  end
+
+  def build_shortened_url
+    self.shortened_url = "#{HOST_URL}/#{code_url}"
+  end
+end
---
+++ spec/models/shortened_url_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe ShortenedUrl, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:full_url) }
+  end
+end
