84b71bec1ebabc12f5e7f7c47ef6d053a03801d5
David Jarillo <vjdavid621@gmail.com>
2021-09-27 12:21:08 -0700

    refactoring inspect web service

--- app/services/inspect_web_service.rb
+++ app/services/inspect_web_service.rb
@@ -1,37 +1,32 @@
 class InspectWebService
   include Interactor
 
-  delegate :full_url, :short_link, to: :context
+  delegate :full_url, to: :context
 
   def call
-    get_title
+    context.title = get_title_tag_content
   end
 
   private
 
   def perform_request
-    @perform_request ||= begin
-      response = Faraday.get(full_url)
-      context.fail! message: 'there was an error with the url' unless response.success?
-      response
-    end
+    response = Faraday.get(full_url)
+    context.fail! message: 'There was an error with the given url' unless response.success?
+    response
   end
 
   def parse_request
     @parse_request ||= begin
-      context.fail! message: 'there was a problem trying to parse the website' unless perform_request
+      context.fail! message: 'There was a problem trying to parse the DOM content' unless perform_request
       Nokogiri::HTML(perform_request.body)
     end
   end
 
-  def get_title
-    return unless parse_request
-    parse_request.title
-  end
+  def get_title_tag_content
+    unless parse_request.instance_of?(Nokogiri::HTML4::Document)
+      context.fail! message: 'There was an error trying to get the data from the html title tag'
+    end
 
-  def save_title
-    short_link.title = get_title
-    context.fail! message: short_link.errors.messages.to_s unless short_link.valid?
-    shot_link.save
+    parse_request.title
   end
 end
